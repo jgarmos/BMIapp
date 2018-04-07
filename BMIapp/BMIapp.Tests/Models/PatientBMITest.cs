841531323c1e1d6f78fb4376793f17c61e118884
Javier Garcia <javiergarciaramos@gmail.com>
2018-04-07 20:37:37 +0100

    Added PatientBMI model, and ApplicationUser and tests

---
+++ BMIapp/BMIapp.Tests/Models/PatientBMITest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMIapp;
+using BMIapp.Models;
+using BMIapp.Controllers;
+
+namespace BMIapp.Tests.Controllers
+{
+    [TestClass]
+    public class PatientBMITest
+    {
+        [TestMethod]
+        public void CalculateBMI_GivenPatientWeightAndHeight_returnCorrectBMInumber()
+        {
+            //Arrange
+            double weight = 68;
+            double height = 1.64;
+            double expectedBMI = 25.3;
+
+            //Act
+            double result = new PatientBMI(weight, height).BMI;
+
+            //Assert
+            Assert.AreEqual(expectedBMI, result);
+        }
+
+        [TestMethod]
+        public void CalculateBMIcategory_GivenPatient70kg2m_returnUnderweight()
+        {
+            //Arrange
+            double weight = 70;
+            double height = 2;
+            double expectedBMI = 17.5;
+            string expectedBMIcategory = "Underweight";
+
+            //Act
+            var result = new PatientBMI(weight, height);
+
+            //Assert
+            Assert.AreEqual(expectedBMI, result.BMI);
+            Assert.AreEqual(expectedBMIcategory, expectedBMIcategory);
+        }
+
+        [TestMethod]
+        public void CalculateBMIcategory_GivenPatient80kg2m_returnUnderweight()
+        {
+            //Arrange
+            double weight = 80;
+            double height = 2;
+            double expectedBMI = 20;
+            string expectedBMIcategory = "Normal Weight";
+
+            //Act
+            var result = new PatientBMI(weight, height);
+
+            //Assert
+            Assert.AreEqual(expectedBMI, result.BMI);
+            Assert.AreEqual(expectedBMIcategory, expectedBMIcategory);
+        }
+
+        [TestMethod]
+        public void CalculateBMIcategory_GivenPatient100kg2m_returnUnderweight()
+        {
+            //Arrange
+            double weight = 100;
+            double height = 2;
+            double expectedBMI = 25;
+            string expectedBMIcategory = "Overweight";
+
+            //Act
+            var result = new PatientBMI(weight, height);
+
+            //Assert
+            Assert.AreEqual(expectedBMI, result.BMI);
+            Assert.AreEqual(expectedBMIcategory, expectedBMIcategory);
+        }
+
+        [TestMethod]
+        public void CalculateBMIcategory_GivenPatient130kg2m_returnUnderweight()
+        {
+            //Arrange
+            double weight = 130;
+            double height = 2;
+            double expectedBMI = 32.5;
+            string expectedBMIcategory = "Obesity";
+
+            //Act
+            var result = new PatientBMI(weight, height);
+
+            //Assert
+            Assert.AreEqual(expectedBMI, result.BMI);
+            Assert.AreEqual(expectedBMIcategory, expectedBMIcategory);
+        }
+
+        [TestMethod]
+        public void CalculateBMIcategory_GivenPatient200kg2m_returnUnderweight()
+        {
+            //Arrange
+            double weight = 200;
+            double height = 2;
+            double expectedBMI = 50;
+            string expectedBMIcategory = "not categorized";
+
+            //Act
+            var result = new PatientBMI(weight, height);
+
+            //Assert
+            Assert.AreEqual(expectedBMI, result.BMI);
+            Assert.AreEqual(expectedBMIcategory, expectedBMIcategory);
+        }
+    }
+}
---
+++ BMIapp/BMIapp/Models/PatientBMI.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BMIapp.Models
+{
+    public class PatientBMI
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+
+        [Required]
+        public double Height { get; set; }
+
+        public double BMI
+        {
+            get { return CalculateBMI(this); }
+        }
+
+        public string Category {
+            get { return CalculateBMIcategory(this.BMI); }
+        }
+
+
+        public PatientBMI (double weight, double height)
+        {
+            Weight = weight;
+            Height = height;
+        }
+
+
+        private double CalculateBMI(PatientBMI patient)
+        {
+            double bmi = patient.Weight / Math.Pow(patient.Height, 2);
+            double roundedBmi = Math.Round(bmi, 1);
+
+            return roundedBmi;
+        }
+
+        private string CalculateBMIcategory (double bmi)
+        {
+            string category;
+
+            if (bmi < 18.5)
+            {
+                return "Underweight";
+            }
+            if (bmi >= 18.5 && bmi <= 24.9)
+            {
+                return "Normal Weight";
+            }
+            if (bmi >= 25.5 && bmi <= 29.9)
+            {
+                return "Pre-obesity";
+            }
+            if (bmi >= 30 && bmi <= 34.9)
+            {
+                return "Obesity class I";
+            }
+            else
+            {
+                return "not categorized";
+            }
+
+        }
+
+
+    }
+   
+}
